cf65ad732b9f741755f9dab2c6fb1cfca0d7e33d
Jamie Andre <jamienunez02@gmail.com>
2023-07-12 11:44:11 -0500

    Errores

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import "./styles/index.css";
+import "./index.css";
+import App from "./components/app/App";
 
-const router = createHashRouter([{ path: "/*", element: <App /> }]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createHashRouter([{ path: "/*", element: <App /> } ]);
+
+root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>
